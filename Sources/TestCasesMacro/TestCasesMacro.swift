8f881c0134b8811b55d42974731d3905229d5628
Alim Yuzbashev <45521753+alimyuz@users.noreply.github.com>
2023-06-13 20:19:30 +0200

    fix naming

--- Sources/TestCasesMacro/TestCasesMacro.swift
+++ Sources/TestCasesMacro/TestCasesMacro.swift
@@ -1,2 +1,2 @@
-@attached(peer, names: overloaded)
+@attached(peer, names: arbitrary)
 public macro testCase<each T>(_: repeat each T) = #externalMacro(module: "TestCasesMacroMacros", type: "TestCaseMacro")

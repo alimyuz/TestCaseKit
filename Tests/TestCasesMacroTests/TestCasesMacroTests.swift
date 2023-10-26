724f1a73b5e667b5c44869421ed105325aa86258
Alim Yuzbashev <45521753+alimyuz@users.noreply.github.com>
2023-10-26 19:53:39 +0200

    add more tests

--- Tests/TestCasesMacroTests/TestCasesMacroTests.swift
+++ Tests/TestCasesMacroTests/TestCasesMacroTests.swift
@@ -2,6 +2,7 @@ import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
 import XCTest
 import TestCasesMacroMacros
+import TestCasesMacro
 
 let testMacros: [String: Macro.Type] = [
     "testCase": TestCaseMacro.self,
@@ -211,5 +212,24 @@ final class TestCasesMacroTests: XCTestCase {
             ],
             macros: testMacros
         )
+    }
+    
+    @testCase(3, 1, 4)
+    @testCase(5, 3, 8)
+    @testCase(6, 12, 18)
+    func testRealCodeMacroSum(_ lhs: Int, _ rhs: Int, _ result: Int) {
+        XCTAssertEqual(lhs + rhs, result)
+    }
+    
+    @testCase("1", "1", "11")
+    @testCase("5", "3", "53")
+    @testCase("6", "12", "612")
+    func testRealCodeMacroSumStrings(_ lhs: String, _ rhs: String, _ result: String) {
+        XCTAssertEqual(lhs + rhs, result)
+    }
+    
+    @testCase(nil as String?)
+    func testRealCodeMacroOptional(_ x: String?) {
+        XCTAssertNil(x)
     }
 }
